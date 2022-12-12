0ef3c4625222498653b7c52928b9d786a6828ae5
chaithran <chaithra.ediga@gmail.com>
2022-12-12 14:39:15 +0100

    Add project files.

---
+++ BloggingAPI/Data/BloggingAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BloggingAPI.Models;
+
+namespace BloggingAPI.Data
+{
+    public class BloggingAPIContext : DbContext
+    {
+        public BloggingAPIContext (DbContextOptions<BloggingAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BloggingAPI.Models.User> User { get; set; } = default!;
+    }
+}
---
+++ BloggingAPI/Models/Blogs.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloggingAPI.Models
+{
+    public class Blog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Blog name is required")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required(ErrorMessage ="Owner is not assigned for the Blog")]
+        public User Owner { get; set; }
+    }
+    public class BlogPost : Blog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required,MinLength(1,ErrorMessage = "Content should have atleast one character")]
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public Comment[]? Comments { get; set; }
+        public Tag[]? Tags { get; set; }
+    }
+
+    public class Comment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, MinLength(1, ErrorMessage = "Comments should have atleast one character")]
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+    public class Tag
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        [Required, StringLength(30,MinimumLength =3, ErrorMessage = "User name must contain minimum 3 characters and upto 30 characters")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+
+}
